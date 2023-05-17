b22e6088732fdab067e15c55aaec58e7e90c5a08
lizi <3132788266@qq.com>
2023-05-17 20:39:10 +0800

    Site updated: 2023-05-17 20:39:10

--- js/runtime.js
+++ js/runtime.js
@@ -1,8 +1,8 @@
 var now = new Date; 
 function createtime() { now.setTime(now.getTime() + 1e3); 
-    var e = new Date("17/08/2023 00:00:00"), 
+    var e = new Date("17/05/2023 00:00:00"), 
     t = Math.trunc(234e8 + (now - e) / 1e3 * 17), 
-    a = (t / 1496e5).toFixed(6), o = new Date("17/08/2023 00:00:00"), 
+    a = (t / 1496e5).toFixed(6), o = new Date("17/05/2023 00:00:00"), 
     n = (now - o) / 1e3 / 60 / 60 / 24, r = Math.floor(n), i = (now - o) / 1e3 / 60 / 60 - 24 * r, s = Math.floor(i); 
     1 == String(s).length && (s = "0" + s); var d = (now - o) / 1e3 / 60 - 1440 * r - 60 * s, l = Math.floor(d); 
     1 == String(l).length && (l = "0" + l); var g = (now - o) / 1e3 - 86400 * r - 3600 * s - 60 * l, b = Math.round(g); 
